78f4e3a2ba3489a70ff836a205e4afdd0d1f995d
angel <egenad8@gmail.com>
2024-09-04 13:47:21 +0200

    Gradle SDK Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "es.atm.gbee"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.atm.gbee"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
