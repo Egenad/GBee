c7d2f0a390d927f288ba928d4524aaa8302386f8
angel <egenad8@gmail.com>
2024-09-14 13:59:46 +0200

    unit tests

--- app/src/test/java/es/atm/gbee/modules/CPUTest.kt
+++ app/src/test/java/es/atm/gbee/modules/CPUTest.kt
@@ -5,6 +5,45 @@ import org.junit.Assert.assertFalse
 import org.junit.Test
 
 class CPUTest {
+
+    @Test
+    fun testLd_BC_NN() {
+        CPU.PC = 0x101
+        Memory.writeByteOnAddress(0x101, 0xFF.toByte())
+        Memory.writeByteOnAddress(0x102, 0xF0.toByte())
+
+        CPU.ld_bc_nn()
+
+        assertEquals(0xF0.toByte(), CPU.B)
+        assertEquals(0xFF.toByte(), CPU.C)
+    }
+
+    @Test
+    fun testOpcodeExecute() {
+        CPU.PC  = 0x0
+
+        Memory.writeByteOnAddress(0x0, 0x01.toByte())
+        Memory.writeByteOnAddress(0x1, 0xFF.toByte())
+        Memory.writeByteOnAddress(0x2, 0xF0.toByte())
+
+        val result = CPU.step()
+
+        assertEquals(0xF0.toByte(), CPU.B)
+        assertEquals(0xFF.toByte(), CPU.C)
+        assertEquals(result, true)
+    }
+
+    @Test
+    fun testLd_BC_A() {
+        CPU.B = 0x10.toByte()
+        CPU.C = 0xFF.toByte()
+        CPU.A = 0x80.toByte()
+
+        CPU.ld_bc_a()
+
+        assertEquals(0x80.toByte(), Memory.getByteOnAddress(0x10FF))
+    }
+
     @Test
     fun testAddA_B() {
         CPU.A = 0x10.toByte()
